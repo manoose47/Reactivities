d8fec37c94574dc7b8dfd01ab0ccfb3e88e95d2e
manoose47 <manoose47@gmail.com>
2021-08-05 23:22:03 +0100

    Add Activity Form

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import { Button, Card, Image } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import React from "react";
+import { Button, Card, Image } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
 
 interface Props {
-    activity: Activity;
+  activity: Activity;
 }
 
-
 export default function ActivityDetails({ activity }: Props) {
-    return (
-        <Card>
-            <Image src={`/assets/categoryImages/{activity.category}.jpg`} />
-            <Card.Content>
-                <Card.Header>{activity.title}</Card.Header>
-                <Card.Meta>
-                    <span>{activity.date}</span>
-                </Card.Meta>
-                <Card.Description>
-                    {activity.description}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <Button.Group widths='2'>
-                    <Button basic color='blue' content='Edit'></Button>
-                    <Button basic color='grey' content='Cancel'></Button>
-                </Button.Group>
-            </Card.Content>
-        </Card>
-    )
+  return (
+    <Card fluid>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths="2">
+          <Button basic color="blue" content="Edit"></Button>
+          <Button basic color="grey" content="Cancel"></Button>
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
 }
